dd2399b678c8212b8f9987679f6addd2ccc0c05e
ProjetInfoAlexandre <apytio3@gmail.com>
2018-01-16 08:38:31 +0100

    Push classe lvl 1

---
+++ Tankiller/Tankiller/Tankiller/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tankiller
+{
+    class Entity
+    {
+        private int x { get; set; }
+        private int y { get; set; }
+
+
+    }
+}
---
+++ Tankiller/Tankiller/Tankiller/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tankiller
+{
+    class Game
+    {
+        private Entity[][] entities;
+
+        private List<Missile> missiles = new List<Missile>();
+        public Game()
+        {
+
+        }
+
+        public void shootMissile(int x, int y, Tank tank)
+        {
+
+        }
+    }
+}
